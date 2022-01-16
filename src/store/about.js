13b00d0108e1913c529cf91448fcddd6d7df8514
saran <saran4113@thekary.com>
2022-01-16 18:03:51 +0900

    create project

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name:'HEROPY',
+    email: 'thekary@gamil.com',
+    blog:'https://heropy.blog',
+    phone:'+82-10-9999-3333',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+
+}
